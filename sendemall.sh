3883568190bfe4efbc985b1c0ca005657aef52df
mitchwyle <mfw@wyle.org>
2016-12-20 16:20:56 -0800

    adding a send to list bash script

---
+++ sendemall.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Send email to a list, one message at a time
+
+export MSGFILE=tm
+
+die() {
+  echo $1
+  exit 1
+}
+
+main() {
+  [ -f $1 ] || die "WTF: No list of addresses, exiting."
+  cat $1 | 
+  while read addr ; do
+    echo "To: $addr" | cat - $MSGFILE | /usr/sbin/sendmail -oi -t
+    echo "sending $MSGFILE to $addr . . ."
+  done
+}
+
+main $1
