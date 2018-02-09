b5aaab032f12762a9968d2b8fa1bda7062a25ca5
Wyle <mwyle@lm-sea-11005813.corp.ebay.com>
2018-02-09 15:28:35 -0800

    adding cal3; updating sendmail.sh

--- sendemall.sh
+++ sendemall.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 # Send email to a list, one message at a time
-# requires a mail transport such as postscript, e.g. sudo apt-get install postscript
+# requires a mail transport such as postfix, e.g. sudo apt-get install postscript
+#
 
-export MSGFILE=tm # file containing message including From, Subject
+export MSGFILE=tm 	# file containing message including From, Subject
 
-die() {		  # helper function
+die() {		  	# helper function
   echo $1
   exit 1
 }
@@ -14,7 +15,7 @@ main() {
   [ -f $1 ] || die "WTF: No list of addresses, exiting."
   cat $1 | 
   while read addr ; do
-    echo "To: $addr" | cat - $MSGFILE | /usr/sbin/sendmail -oi -t
+    echo "To: $addr"",mwyle@ebay.com" | cat - $MSGFILE | /usr/sbin/sendmail -oi -t
     echo "sending $MSGFILE to $addr . . ."
   done
 }
