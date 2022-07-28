4d5fc9a42d65e543579c6d5248f91e32c2a736e3
mitchellwyle <mitchell.wyle@lessen.com>
2022-07-28 14:45:00 -0700

    creating repo to demonstrate bash coding concdpts

--- killall/killall.sh
+++ killall/killall.sh
@@ -1,38 +1,32 @@
 #
-# killall bash function & unit tests
+# killall reimplementation of command in bash
 
 
+# killall "program" -- Kill all processes with the name "program"
+#
 
-# Kill all processes with the name "program"
 function shkillall() {
-  local USAGE="killall program"
+  local USAGE="Usage: $0 program"
   # Verify we have exactly one parameter, the name of the program we want to kill:
   case $# in
-   1) break ;;
-   *) echo "Wrong number of arguments\n" 
+   1) x=x ;;
+   *) echo "Wrong number of parameters." 
       echo $USAGE 
       exit 3 ;;
   esac
 
+  $DBUG && set -x
   ps aux | 
-  killall $1
   grep $1 |
   grep -v grep |
-  awk '{ print $2 }'
-  # xargs kill 
+  awk '{ print $2 }' |
+  xargs kill 
 }
 
 
 main() {
-  local USAGE="$0 program"
-  # Verify we have exactly one parameter, the name of the program we want to kill:
-  case $# in
-   1) break ;;
-   *) echo "Wrong number of arguments\n" 
-      echo $USAGE 
-      exit 3 ;;
-  esac
-
-  shkillall 
-  
+  shkillall $@
 }
+
+export DBUG=false
+main $@
